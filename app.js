aa88c850b257a97b9c96ea1a34013c90a16c54da
Long G <long.w.guan@gmail.com>
2022-10-23 23:18:26 -0700

    Added js creation for confirm delete pop up for each book

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ function addBookContainer(input) {
     addCheckBox(newBook, input.checkbox);
     addBookInfo(newBook, "date", "Published: " + input.date);
     addSVG(newBook);
+    newBook.appendChild(createConfirmPopup())
     bookContainer.appendChild(newBook);
 }
 
@@ -121,26 +122,48 @@ function addDelete() {
     newSVGPath.setAttribute("fill", "currentColor");
     newSVGPath.setAttribute("d", "M19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3M19,19H5V5H19V19M17,8.4L13.4,12L17,15.6L15.6,17L12,13.4L8.4,17L7,15.6L10.6,12L7,8.4L8.4,7L12,10.6L15.6,7L17,8.4Z");
     newSVG.appendChild(newSVGPath);
-    newSVG.addEventListener("click", ()=>{
-        showConfirmDelete(newSVG);
-    });
+    newSVG.addEventListener("click", showConfirmDelete);
     return newSVG;
 }
 
 // displays the confirm pop up
-function showConfirmDelete(newSVG) {
-    confirmDelete.style.display = "flex";
-    bookContainer.classList.add("blur");
-    const confirmDeleteNo = document.querySelector(".confirm-no").addEventListener("click", ()=>{
-        hideConfirmDelete();
+function showConfirmDelete(evt) {
+    evt.currentTarget.parentNode.nextElementSibling.style.display = "flex"; // displays confirm-delete popup
+    evt.currentTarget.parentNode.nextElementSibling.children[1].children[1].addEventListener("click", ()=>{ // selects no button
+        this.parentNode.nextElementSibling.style.display = "none";
+        console.log('no');
     });
-    const confirmDeleteYes = document.querySelector(".confirm-yes").addEventListener("click", ()=>{
-        newSVG.parentNode.parentNode.remove();
-        hideConfirmDelete();
+    evt.currentTarget.parentNode.nextElementSibling.children[1].children[0].addEventListener("click", ()=>{ // selects no button
+        console.log('yes');
     });
 }
 
-function hideConfirmDelete() {
-    confirmDelete.style.display = "none";
-    bookContainer.classList.remove("blur");
+function removeSVG(evt) {
+    console.log(evt.currentTarget);
+    evt.currentTarget.parentNode.parentNode.remove();
+    hideConfirmDelete();
+}
+
+
+// creates indivudla confirm popup for each book
+function createConfirmPopup() {
+    const newContainer = document.createElement('div');
+    newContainer.classList.add("confirm-delete");
+    const newDiv = document.createElement('div');
+    const content = document.createTextNode("Are you sure you want to delete this book?");
+    newDiv.appendChild(content);
+    const newDiv2 = document.createElement('div');
+    const yesButton = document.createElement("button");
+    yesButton.classList.add("confirm-yes");
+    const yesContent = document.createTextNode("Yes");
+    yesButton.appendChild(yesContent);
+    const noButton = document.createElement("button");
+    const noContent = document.createTextNode("No");
+    noButton.appendChild(noContent);
+    noButton.classList.add("confirm-no");
+    newDiv2.appendChild(yesButton);
+    newDiv2.appendChild(noButton);
+    newContainer.appendChild(newDiv);
+    newContainer.appendChild(newDiv2);
+    return newContainer;
 }
